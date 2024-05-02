7cba6cdd645b7289bdd1e866a1755f4acb43316e
Akith-002 <chandinubaa.22@uom.lk>
2024-05-02 13:58:37 +0530

    recipe card styling

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -7,33 +7,35 @@ const RecipeCard = ({ recipe }) => {
   console.log(recipe);
   return (
     <div id="recipeCard">
-      <h5>{recipe.id}</h5>
-      <p>{recipe.name}</p>
+      {/* <h5>{recipe.id}</h5> */}
 
       <div id="card-img">
         <img src={recipe.imageURL} alt={recipe.name} />
       </div>
-      <br />
-      {recipe.category &&
-        recipe.category.map((cat) => {
-          return (
-            <>
-              <button
-                onClick={() => {
-                  // setURL("http://localhost:3000/recipes?category=option3");
-                  setURL("http://localhost:3000/recipes");
-                }}
-                className="tags"
-              >
-                {cat}
-              </button>
-            </>
-          );
-        })}
-      <br />
-      <Link to={`/recipes/${recipe.id}`}>
-        <button>View Recipe</button>
-      </Link>
+
+      <div id="card-container">
+        <p>{recipe.name}</p>
+        {recipe.category &&
+          recipe.category.map((cat) => {
+            return (
+              <>
+                <button
+                  onClick={() => {
+                    // setURL("http://localhost:3000/recipes?category=option3");
+                    setURL("http://localhost:3000/recipes");
+                  }}
+                  className="tags"
+                >
+                  {cat}
+                </button>
+              </>
+            );
+          })}
+        <br />
+        <Link to={`/recipes/${recipe.id}`}>
+          <button>View Recipe</button>
+        </Link>
+      </div>
     </div>
   );
 };
