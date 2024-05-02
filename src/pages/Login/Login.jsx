43238001520461457636bff028a0065433fd4c36
rithakith <ritharaedirisinghe@gmail.com>
2024-05-02 12:32:05 +0530

    added login page styling

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,29 +2,43 @@ import React from 'react'
 
 import './Login.css'
 import Navbar from '../../components/Navbar/Navbar'
-
+import leftImg from '../../assets/images/loginImg.png'
 const Login = () => {
   return (
     <>
     <Navbar/>
-    <div class="container">
-      <div class="wrapper">
-        <div class="title"><span>Login Form</span></div>
-        <form action="#">
-          <div class="row">
-            <i class="fas fa-user"></i>
-            <input type="text" placeholder="Email or Phone" required />
-          </div>
-          <div class="row">
-            <i class="fas fa-lock"></i>
-            <input type="password" placeholder="Password" required/>
-          </div>
-          <div class="pass"><a href="#">Forgot password?</a></div>
-          <div class="row button">
-            <input type="submit" value="Login"/>
-          </div>
-          <div class="signup-link">Not a member? <a href="/signup">Signup now</a></div>
-        </form>
+    <div class="containerLogin">
+      <div class="leftLogin">
+        <img src={leftImg} alt="" />
+      </div>
+      <div class="rightLogin">
+        <div class="contentLogin">
+          <h1>Welcome Back!!</h1>
+
+          <form action="">
+            <div class="name">
+              <label for="name">
+                Username:
+                <br />
+                <input type="text" name="" id="name" placeholder="Username" />
+              </label>
+            </div>
+            <div class="passwordDiv">
+              <label for="password">
+                Password:
+                <br />
+                <input type="password" name="" id="password" placeholder="Password" />
+              </label>
+            </div>
+
+            <div class="buttonsLogin">
+              <input type="submit" value="Login" id="loginButton" />
+              <input type="submit" value="Cancel" id="cancelButton" />
+            </div>
+          </form>
+          Don't have an account? then 
+          <span id="SignupSpan"> <a href="'/signup" id="signupButton"> Sign up</a></span>
+        </div>
       </div>
     </div>
 
