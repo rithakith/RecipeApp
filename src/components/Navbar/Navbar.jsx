8e2278b45ee3a5b6859c4299623e14b3f438f7bf
Akith-002 <chandinubaa.22@uom.lk>
2024-05-02 11:42:08 +0530

    navbar adjustments

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,42 +6,41 @@ import logo from "../../assets/Gemini_Generated_Image_jsac2kjsac2kjsac.jpeg";
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <div className="navContainer">
-          <ul id="navUL">
-            <div className="logo">
-              <li>
-                <Link to={"/"}>
-                  <img src={logo} alt="logo" />
-                </Link>
-              </li>
-            </div>
-            <div className="remainingNav">
-              <li>
-                <Link to={"/recipes"} style={{ textDecoration: "none" }}>
-                  Recipes
-                </Link>
-              </li>
-              <li>
-                <Link to={"/newrecipe"} style={{ textDecoration: "none" }}>
-                  Create Recipe
-                </Link>
-              </li>
-              <li>Signup</li>
-              <li
-                style={{
-                  backgroundColor: "green",
-                  color: "white",
-                  border: "none",
-                }}
-              >
-                <Link to={"/login"} style={{ color: "white" }}>
-                  Login
-                </Link>
-              </li>
-            </div>
-          </ul>
-        </div>
+      <nav className="navContainer">
+        <ul id="navUL">
+          <div className="logo">
+            <li>
+              <Link to={"/"}>
+                <img src={logo} alt="logo" />
+              </Link>
+            </li>
+          </div>
+
+          <div className="remainingNav">
+            <li>
+              <Link to={"/recipes"} style={{ textDecoration: "none" }}>
+                Recipes
+              </Link>
+            </li>
+            <li>
+              <Link to={"/newrecipe"} style={{ textDecoration: "none" }}>
+                Create Recipe
+              </Link>
+            </li>
+            <li>Signup</li>
+            <li
+              style={{
+                backgroundColor: "green",
+                color: "white",
+                border: "none",
+              }}
+            >
+              <Link to={"/login"} style={{ color: "white" }}>
+                Login
+              </Link>
+            </li>
+          </div>
+        </ul>
       </nav>
     </>
   );
