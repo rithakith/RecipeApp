c0351bffdb62ea5ccaeea9e7507741321f4d0c94
Akith-002 <chandinubaa.22@uom.lk>
2024-05-02 14:13:04 +0530

    recipe card updates

--- src/pages/RecipeCollection/RecipeCollection.jsx
+++ src/pages/RecipeCollection/RecipeCollection.jsx
@@ -20,28 +20,42 @@ const RecipeCollection = () => {
     <>
       <Navbar />
       <br />
-      <button onClick={() => handleButtonClick("http://localhost:3000/recipes")}>
+      <button
+        onClick={() => handleButtonClick("http://localhost:3000/recipes")}
+      >
         All
       </button>
-      <button onClick={() => handleButtonClick("http://localhost:3000/recipes?category=option1")}>
+      <button
+        onClick={() =>
+          handleButtonClick("http://localhost:3000/recipes?category=option1")
+        }
+      >
         Option 1
       </button>
-      <button onClick={() => handleButtonClick("http://localhost:3000/recipes?category=option2")}>
+      <button
+        onClick={() =>
+          handleButtonClick("http://localhost:3000/recipes?category=option2")
+        }
+      >
         Option 2
       </button>
-      <button onClick={() => handleButtonClick("http://localhost:3000/recipes?category=option3")}>
+      <button
+        onClick={() =>
+          handleButtonClick("http://localhost:3000/recipes?category=option3")
+        }
+      >
         Option 3
       </button>
 
       {isPending && <div>Loading...</div>}
       {error && <div>{error}</div>}
 
-      {recipes &&
-        recipes.map((recipe, index) => {
-          return (
-            <RecipeCard key={index} recipe={recipe} />
-          );
-        })}
+      <div id="collection-container">
+        {recipes &&
+          recipes.map((recipe, index) => {
+            return <RecipeCard key={index} recipe={recipe} />;
+          })}
+      </div>
     </>
   );
 };
