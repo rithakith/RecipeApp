a70e296dfb90827021470282e46aca75d6fc1ea5
JalinaH <gunathungagajh.22@uom.lk>
2024-05-03 21:20:14 +0530

    footer and bootstrap

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import { Carousel } from "bootstrap";
+
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <div>Home</div>
+      <Footer />
     </>
   );
 };
