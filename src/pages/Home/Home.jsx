c4ec23d4d60864c5b0c56f48b90f1f8ac69bc613
Akith-002 <chandinubaa.22@uom.lk>
2024-05-03 18:06:10 +0530

    styling updates

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import sliderimg1 from "../../assets/Carousel/Slider_img1.jpg";
+import sliderimg2 from "../../assets/Carousel/Slider_img2.jpg";
+import sliderimg3 from "../../assets/Carousel/Slider_img3.jpg";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div>Home</div>
+      <div id="home-container">
+        <div id="home-section1">Carousel</div>
+      </div>
     </>
   );
 };
