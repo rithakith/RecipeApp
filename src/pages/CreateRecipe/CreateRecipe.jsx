25e8ac5c70aaf6b3b1f20679f6a4edd1d115c88c
rithakith <ritharaedirisinghe@gmail.com>
2024-05-03 13:09:37 +0530

    backend of create recipe page

--- src/pages/CreateRecipe/CreateRecipe.jsx
+++ src/pages/CreateRecipe/CreateRecipe.jsx
@@ -1,22 +1,75 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import useFetch from "../../Hooks/useFetch";
+
+import "./CreateRecipe.css";
 
 const CreateRecipe = () => {
   const [title, setTitle] = useState("");
-  const [steps, setSteps] = useState("");
+  const [newStep, setNewStep] = useState("");
+  const [steps, setSteps] = useState([]);
   const [newIngredient, setNewIngredient] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const ingredientInput = useRef(null);
+  const stepInput = useStef(null);
+  const [category, setCategory] = useState("option1");
+  const [owner, setOwner] = useState("");
+  const [image, setImage] = useState("");
+
+  const { postData, data, error } = useFetch(
+    "http://localhost:3000/recipes",
+    "POST"
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hey");
-    console.log(title, steps, newIngredient);
+    console.log(
+      title,
+      steps,
+      newIngredient,
+      ingredients,
+      category,
+      owner,
+      image
+    );
+    postData({
+      name: title,
+      owner,
+      category: [category],
+      ingredients,
+      steps,
+      imageURL: image,
+    });
+  };
+
+  const handleIngAdd = (e) => {
+    e.preventDefault();
+    const ing = newIngredient.trim();
+
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients((prevIngredients) => [...prevIngredients, ing]);
+    }
+
+    setNewIngredient("");
+    ingredientInput.current.focus();
+  };
+  const handleStepAdd = (e) => {
+    e.preventDefault();
+    const step = newStep.trim();
+
+    if (step && !steps.includes(step)) {
+      setSteps((prevSteps) => [...prevSteps, step]);
+    }
+
+    setNewStep("");
+    stepInput.current.focus();
   };
 
   return (
     <>
       <Navbar />
       <br />
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label>
           Recipe name:
           <input
@@ -25,15 +78,33 @@ const CreateRecipe = () => {
               setTitle(e.target.value);
             }}
             value={title}
+            required
+          />
+        </label>
+        <br />
+        <label>
+          Name of owner:
+          <input
+            type="text"
+            onChange={(e) => {
+              setOwner(e.target.value);
+            }}
+            value={owner}
+            required
           />
         </label>
         <br />
         <label>
           Category:
-          <select>
-            <option value="1">Option1</option>
-            <option value="2">Option2</option>
-            <option value="3">Option3</option>
+          <select
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+            value={category}
+          >
+            <option value="option1">Option1</option>
+            <option value="option2">Option2</option>
+            <option value="option3">Option3</option>
           </select>
         </label>
         <div>
@@ -48,15 +119,32 @@ const CreateRecipe = () => {
                     setNewIngredient(e.target.value);
                   }}
                   value={newIngredient}
+                  ref={ingredientInput}
+
                 />
-                <button onClick={() => {}}>Add</button>
+                <button onClick={handleIngAdd}>Add</button>
               </label>
             </li>
           </ol>
+          <p>Current Ingredients :</p>
+          {ingredients.map((ing) => {
+            return <li key={ing}>{ing}</li>;
+          })}
         </div>
         <br />
         <label>
-          Method: <br />
+          Image URL:
+          <input
+            type="text"
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+            required
+            value={image}
+          />
+        </label>
+        {/* <label>
+          Directions: <br />
           <textarea
             name=""
             id=""
@@ -66,8 +154,34 @@ const CreateRecipe = () => {
               setSteps(e.target.value);
             }}
             value={steps}
+            required
           ></textarea>
-        </label>
+        </label> */}
+        <div>
+          Steps:
+          <ol>
+            <li>
+              <label>
+                {" "}
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setNewStep(e.target.value);
+                  }}
+                  value={newStep}
+                  ref={stepInput}
+                  required
+                />
+                <button onClick={handleStepAdd}>Add</button>
+              </label>
+            </li>
+          </ol>
+          <p>Current Steps :</p>
+          {steps.map((step) => {
+            return <li key={step}>{step}</li>;
+          })}
+        </div>
+        <br />
         <br />
         <button
           onClick={() => {
