7513951827113528b41e0a3d6b47ef272caa7e04
rithakith <ritharaedirisinghe@gmail.com>
2024-05-05 20:38:08 +0530

    added recommended and favourites

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import Carousel from "../../components/Carousel/Carousel";
 import { CaretRight } from "@phosphor-icons/react";
 import Modal from "../../components/Modal/Modal";
-
+import useFetch from "../../Hooks/useFetch";
+import RecipeCard from "../../components/RecipeCard/RecipeCard";
 const Home = () => {
+  // Today's special
+  const [urlFavourites, setURLFavourites] = useState(
+    "http://localhost:3000/favourites"
+  );
+
+  const {
+    data: favourites,
+    isPending: isFavouritesPending,
+    error: favouritesError,
+  } = useFetch(urlFavourites);
+  console.log(favourites);
+  console.log(isFavouritesPending);
+
+  // recommended recipes
+  const [urlRecommended, setURLRecommended] = useState(
+    "http://localhost:3000/recommended"
+  );
+
+  const {
+    data: recommended,
+    isPending: isRecommendedPending,
+    error: recommendedError,
+  } = useFetch(urlRecommended);
+  console.log(recommended);
+  console.log(isRecommendedPending);
+
   return (
     <>
       <Navbar />
@@ -54,14 +81,31 @@ const Home = () => {
 
           <div id="home-container-top">
             <p className="section-topic">Today's Special</p>
+
+            {isFavouritesPending && <div>Loading...</div>}
+            {favouritesError && <div>{error}</div>}
+            <div id="collection-container">
+              {favourites &&
+                favourites.map((recipe, index) => {
+                  return <RecipeCard key={index} recipe={favourites} />;
+                })}
+            </div>
           </div>
 
           <div id="home-container-middle">
             <p className="section-topic">Recommended Recipes</p>
+
+            {isRecommendedPending && <div>Loading...</div>}
+            {recommendedError && <div>{error}</div>}
+            <div id="collection-container">
+              {recommended &&
+                recommended.map((recipe, index) => {
+                  return <RecipeCard key={index} recipe={recommended} />;
+                })}
+            </div>
           </div>
         </div>
       </div>
-
       <Footer />
     </>
   );
