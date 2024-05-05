2a566deb28f4572476dfa08848198b20e1d13f6c
Akith-002 <chandinubaa.22@uom.lk>
2024-05-05 19:28:23 +0530

    profile page styling

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "./ProfilePage.css";
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import profile1 from "../../assets/profile_images/profilepic1.jpg";
 
 const ProfilePage = () => {
   return (
     <>
-      <div>Profile Page</div>
+      <Navbar />
+
+      <div id="profile">
+        <div id="profile-container">
+          <div id="profile-pic-area">
+            <div id="profile-pic-bg">
+              <div id="profile-topic">Profile</div>
+            </div>
+
+            <div id="profile-pic">
+              <img src={profile1} alt="" />
+            </div>
+          </div>
+
+          <div id="profile-details">
+            <div>name</div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </>
   );
 };
