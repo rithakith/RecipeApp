29cc2f44ce8c14884b282caeda98cf0bc0975c30
JalinaH <gunathungagajh.22@uom.lk>
2024-05-05 23:24:21 +0530

    chore: Update RecipeCard key to use index for recommended recipes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -100,7 +100,7 @@ const Home = () => {
             <div id="collection-container">
               {recommended &&
                 recommended.map((recipe, index) => {
-                  return <RecipeCard key={index} recipe={recipe} />;
+                  return <RecipeCard key={index} recipe={recommended} />;
                 })}
             </div>
           </div>
