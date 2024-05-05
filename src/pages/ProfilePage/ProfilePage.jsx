eff110fa9a0855a2f7d761fc7901e10ac9cbbd52
Akith-002 <chandinubaa.22@uom.lk>
2024-05-05 18:33:31 +0530

    profile page init

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ProfilePage.css";
+import Navbar from "../../components/Navbar/Navbar";
+
+const ProfilePage = () => {
+  return (
+    <>
+      <div>Profile Page</div>
+    </>
+  );
+};
+
+export default ProfilePage;
