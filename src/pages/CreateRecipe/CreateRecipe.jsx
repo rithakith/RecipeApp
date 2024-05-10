6f30d3668fd6e72b0640b462f13735f296fb8a8d
rithakith <ritharaedirisinghe@gmail.com>
2024-05-10 11:53:49 +0530

    create Recipe

--- src/pages/CreateRecipe/CreateRecipe.jsx
+++ src/pages/CreateRecipe/CreateRecipe.jsx
@@ -23,14 +23,23 @@ const CreateRecipe = () => {
   const [showModal, setShowModal] = useState(false); // State for showing/hiding modal
   const navigate = useNavigate();
 
-
-
   const handleSubmit = async (e) => {
-    console.log("work")
+    e.preventDefault()
+    console.log("work");
+
+    console.log(
+      title,
+      steps,
+      newIngredient,
+      ingredients,
+      category,
+      owner,
+      image,
+      portions,
+      time
+    );
     const currentDate = new Date();
     const formattedDate = currentDate.toLocaleDateString();
-
-    setLastUpdated(formattedDate);
 
     try {
       await projectFirestore.collection("recipes").add({
@@ -44,20 +53,23 @@ const CreateRecipe = () => {
         time,
         last_update: formattedDate,
       });
-    
 
-    console.log(
-      title,
-      steps,
-      newIngredient,
-      ingredients,
-      category,
-      owner,
-      image,
-      portions,
-      time
-    );
- 
+      console.log(
+        title,
+        steps,
+        newIngredient,
+        ingredients,
+        category,
+        owner,
+        image,
+        portions,
+        time
+      );
+
+// Show modal after submitting
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
     setImage("");
     setTitle("");
     setCategory("option1");
@@ -66,11 +78,8 @@ const CreateRecipe = () => {
     setOwner("");
     setPortions("");
     setTime("");
-    setShowModal(true); // Show modal after submitting
-  }catch(error){
-    console.error("Error adding document: ", error);
+    setShowModal(true); 
   };
-}
 
   const handleIngAdd = (e) => {
     e.preventDefault();
@@ -221,13 +230,7 @@ const CreateRecipe = () => {
         </div>
 
         <div className="create-new">
-          <button
-            className="create-recipe-btn"
-            type="submit"
-            onClick={() => {
-              handleSubmit();
-            }}
-          >
+          <button className="create-recipe-btn" type="submit">
             Create New
           </button>
         </div>
@@ -248,6 +251,5 @@ const CreateRecipe = () => {
     </>
   );
 };
-
 
 export default CreateRecipe;
