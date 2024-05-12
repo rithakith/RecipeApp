90ac443bcd362bf6749f20e27daf1b605c8d7f8a
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-05-12 11:58:41 +0530

    chore: Update dependencies and add email subscription functionality to Footer component

--- src/firebase/welcomeEmail.js
+++ src/firebase/welcomeEmail.js
@@ -1,7 +1,5 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const nodemailer = require("nodemailer");
-const { projectFirestore } = require("./config.js");
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -11,29 +9,26 @@ exports.sendWelcomeEmail = functions.firestore
   .onCreate(async (snap, context) => {
     const email = snap.data().email;
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "jarvis.cookpal@gmail.com",
-        pass: "pobdkkjacgopuukp",
-      },
-    });
-
-    const mailOptions = {
-      from: "jarvis.cookpal@gmail.com",
-      to: email,
-      subject: "Thanks for subscribing!",
-      text: "Welcome to our newsletter. You will now receive the latest recipes and updates.",
-    };
-
+    // Make a POST request to your Apps Script function's web app URL
+    const scriptUrl =
+      "https://script.google.com/macros/s/AKfycbxPRkPcRtv-j6ySuc_gO2qznC3XJyWa9RWHSGi4QeQAcGrwNH9fVxA5Ub3o2093GWSExg/exec"; // Replace with your actual URL
     try {
-      await transporter.sendMail(mailOptions);
-      console.log("Welcome email sent to:", email);
+      const response = await fetch(scriptUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: `email=${encodeURIComponent(email)}`,
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error calling Apps Script: ${response.statusText}`);
+      }
 
       await db
         .collection("subscribers")
         .doc(context.params.subscriberId)
         .update({ welcomeEmailSent: true });
+
+      console.log("Welcome email sent to:", email);
     } catch (error) {
       console.error("Error sending email:", error);
     }
